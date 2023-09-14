33f6c56dbf8ecd9ca30f25169556b891743fe0d5
Luciano Lima <luciano.lima93@outlook.com.br>
2023-09-14 14:32:24 -0300

    feat: change footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <footer className='px-3 w-full py-8 h-full flex flex-col justify-center items-center text-sm dark:text-gray-300 text-gray-950'>
       <SocialMedias className="after:hidden flex-row relative left-auto w-full mb-2 justify-center items-center" />
-      <span className="dark:hover:text-teal-300 hover:text-teal-500 w-full text-center">{t('footer.inspiration')}<a href="https://github.com/bchiang7"
+      {/* <span className="dark:hover:text-teal-300 hover:text-teal-500 w-full text-center">{t('footer.inspiration')}<a href="https://github.com/bchiang7"
         target="_blank"
         className="hover:underline dark:hover:text-teal-300 hover:text-teal-500"
-        rel="noopener noreferrer">{t('footer.author')}</a></span>
+        rel="noopener noreferrer">{t('footer.author')}</a></span> */}
       <span className="dark:hover:text-teal-300 hover:text-teal-500 w-full text-center">
         {t('footer.developed')}
       </span>
